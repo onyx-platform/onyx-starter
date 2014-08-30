a70f75b34563e8e3a25c856dff00b9d5fae27e7a
Michael Drogalis <mjd3089@rit.edu>
2014-08-30 15:40:12 -0700

    0.3.2.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [com.mdrogalis/onyx "0.3.1"]
-                 [com.mdrogalis/onyx-core-async "0.3.1"]])
+                 [com.mdrogalis/onyx "0.3.2"]
+                 [com.mdrogalis/onyx-core-async "0.3.2"]])
