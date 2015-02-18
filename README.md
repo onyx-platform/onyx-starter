9d150e6be1017d25befb6610dd6279001ac1c4be
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-02-18 12:26:02 +0800

    Add dashboard instructions.

--- README.md
+++ README.md
@@ -46,6 +46,29 @@ Evaluate it bit-by-bit in the repl. Tail `onyx.log` for Onyx output.
  :done]
 ```
 
+##### Dashboard
+
+If you would like to use the [Onyx
+Dashboard](https://github.com/lbradstreet/onyx-dashboard/) to inspect the
+cluster, download the
+[uberjar](https://github.com/lbradstreet/onyx-dashboard/#deployment).
+
+As the dashboard is unable to use VM mode HornetQ, you will have to run a standalone HornetQ instance. You can do so by:
+1. ```cd hornetq```
+2. ```./setup-hq.sh```
+3. ```./run-hq.sh ```
+
+In core.clj, alter the line:
+`(def vm-hornetq? true)` 
+to 
+`(def vm-hornetq?  false)`.  
+Then evaluate the sample file bit by bit, as above.
+After the line: 
+```(def env (onyx.api/start-env env-config))``` 
+start the dashboard by:
+
+INSERT JAR RUNNING HERE.
+
 ## License
 
 Copyright © 2014 Michael Drogalis
--- src/onyx_starter/core.clj
+++ src/onyx_starter/core.clj
@@ -4,6 +4,8 @@
             [onyx.plugin.core-async]
             [onyx.api]))
 
+(def vm-hornetq? true)
+
 ;;;;; Implementation functions ;;;;;
 (defn split-by-spaces-impl [s]
   (clojure.string/split s #"\s+"))
@@ -141,42 +143,37 @@
 ;; Use the Round Robin job scheduler
 (def scheduler :onyx.job-scheduler/round-robin)
 
-; (def env-config
-;   {:hornetq/mode :vm
-;    :hornetq.server/type :vm
-;    :hornetq/server? true
-;    :zookeeper/address "127.0.0.1:2186"
-;    :zookeeper/server? true
-;    :zookeeper.server/port 2186
-;    :onyx/id id
-;    :onyx.peer/job-scheduler scheduler})
+(def env-hornetq-config 
+  (if vm-hornetq? 
+    {:hornetq/mode :vm
+     :hornetq.server/type :vm
+     :hornetq/server? true}
+    {:hornetq/mode :standalone
+     :hornetq.standalone/host "127.0.0.1"
+     :hornetq.standalone/port 5445}))
 
-; (def peer-config
-;   {:hornetq/mode :vm
-;    :zookeeper/address "127.0.0.1:2186"
-;    :onyx/id id
-;    :onyx.peer/job-scheduler scheduler})
+(def peer-hornetq-config
+  (if vm-hornetq?
+    {:hornetq/mode :vm}
+    {:hornetq/mode :standalone
+     :hornetq.standalone/host "127.0.0.1"
+     :hornetq.standalone/port 5445}))
 
 (def env-config
-  {:hornetq/mode :standalone
-   :hornetq.standalone/host "127.0.0.1"
-   :hornetq.standalone/port 5445
-   :hornetq/server? false
-   :zookeeper/address "127.0.0.1:2186"
-   :zookeeper/server? true
-   :zookeeper.server/port 2186
-   :onyx/id id
-   :onyx.peer/job-scheduler scheduler})
+  (merge {:zookeeper/address "127.0.0.1:2186"
+          :zookeeper/server? true
+          :zookeeper.server/port 2186
+          :onyx/id id
+          :onyx.peer/job-scheduler scheduler}
+         env-hornetq-config))
 
 (def peer-config
-  {:hornetq/mode :standalone
-   :hornetq.standalone/host "127.0.0.1"
-   :hornetq.standalone/port 5445
-   :zookeeper/address "127.0.0.1:2186"
-   :onyx/id id
-   :onyx.peer/job-scheduler scheduler})
+  (merge {:zookeeper/address "127.0.0.1:2186"
+          :onyx/id id
+          :onyx.peer/job-scheduler scheduler}
+         peer-hornetq-config))
 
-;; Start an in-memory ZooKeeper and standalone HornetQ
+;; Start an in-memory ZooKeeper and HornetQ (depending on vm-hornetq?)
 (def env (onyx.api/start-env env-config))
 
 ;; Start the worker peers.
