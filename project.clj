8f7d72b3765b08a2a297a1c52f5b8bb48c42c87b
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-06-02 10:24:47 -0700

    Delete existing

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject onyx-starter "0.1.0-SNAPSHOT"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main onyx-starter.core
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [org.onyxplatform/onyx "0.6.0-RC1"]])
