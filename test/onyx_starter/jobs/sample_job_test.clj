99234d6e11d52717cd6c06511f4e78c890bf3957
Michael Drogalis <mjd3089@rit.edu>
2015-06-02 19:31:02 -0700

    Bit of clean up.

--- test/onyx_starter/jobs/sample_job_test.clj
+++ test/onyx_starter/jobs/sample_job_test.clj
@@ -1,22 +1,19 @@
 (ns onyx-starter.jobs.sample-job-test
   (:require [clojure.test :refer [deftest is]]
-            [clojure.core.async :refer [>!!]]
-            [clojure.java.io :refer [resource]]
             [com.stuartsierra.component :as component]
-            [onyx-starter.launcher.dev-system :refer [onyx-dev-env]]
-            [onyx-starter.workflows.sample-workflow :refer [workflow]]
-            [onyx-starter.catalogs.sample-catalog :refer [build-catalog] :as sc]
-            [onyx-starter.lifecycles.sample-lifecycle :refer [build-lifecycles] :as sl]
-            [onyx-starter.flow-conditions.sample-flow-conditions :as sf]
-            [onyx-starter.functions.sample-functions]
-            [onyx-starter.dev-inputs.sample-input :as dev-inputs]
             [onyx-starter.launcher.submit-sample-job :as submit-sample]
+            [onyx-starter.launcher.dev-system :refer [onyx-dev-env]]
+            [onyx-starter.functions.sample-functions]
             [onyx.api]))
 
 (deftest test-sample-dev-job
+  ;; 8 peers for 8 distinct tasks in the workflow
   (let [dev-env (component/start (onyx-dev-env 8))]
     (try 
       (let [[loud-out question-out] (submit-sample/submit-job dev-env)]
+        (clojure.pprint/pprint loud-out)
+        (println)
+        (clojure.pprint/pprint question-out)
         (is (= 12 (count question-out)))
         (is (= 12 (count loud-out))))
       (finally 
