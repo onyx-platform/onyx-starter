09b16e6866f807add5a825cc7d7980a8d7bfccca
Michael Drogalis <mjd3089@rit.edu>
2015-06-08 20:25:37 -0700

    Version bump.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.onyxplatform/onyx "0.6.0-RC1"]
+                 [org.onyxplatform/onyx "0.6.0"]
                  [com.stuartsierra/component "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :source-paths ["env/dev" "src"]}})
