4ad318fb35cc9453629cb7b484105d1eecf3552d
Michael Drogalis <mjd3089@rit.edu>
2015-11-11 18:53:48 -0800

    0.8.0 release.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.onyxplatform/onyx "0.8.0-alpha1"]
+                 [org.onyxplatform/onyx "0.8.0"]
                  [com.stuartsierra/component "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :source-paths ["env/dev" "src"]}})
