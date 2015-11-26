2be614af27a905120713a9afcf28e76df48c8910
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-27 03:02:34 +0800

    Upgrade onyx-starter to 0.8.1. Remove conflicting core.async dependency

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.onyxplatform/onyx "0.8.0"]
+                 [org.onyxplatform/onyx "0.8.1"]
                  [com.stuartsierra/component "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :source-paths ["env/dev" "src"]}})
