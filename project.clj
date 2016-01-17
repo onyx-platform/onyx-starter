db543c7b9cf463758676811e97ca530f6481e32f
Michael Drogalis <mjd3089@rit.edu>
2016-01-17 14:24:06 -0800

    Voom-ify Onyx core

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.371"]
-                 [org.onyxplatform/onyx "0.8.4"]
+                 ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
+                 [org.onyxplatform/onyx "0.8.5-20160114_164623-gc81b6ea"]
                  [com.stuartsierra/component "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :plugins [[lein-update-dependency "0.1.2"]
