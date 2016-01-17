bf32e57afe8cc93ad7fac3a24f92817bc14de22a
Michael Drogalis <mjd3089@rit.edu>
2016-01-17 14:16:25 -0800

    Bring in set deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-starter "0.8.4.0"
+(defproject onyx-starter "0.8.4"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
@@ -6,5 +6,6 @@
                  [org.onyxplatform/onyx "0.8.4"]
                  [com.stuartsierra/component "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
-                   :plugins [[lein-update-dependency "0.1.2"]]
+                   :plugins [[lein-update-dependency "0.1.2"]
+                             [lein-set-version "0.4.1"]]
                    :source-paths ["env/dev" "src"]}})
