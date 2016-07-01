f3d60e9b997a7349cf8ef686e7f0d3475d42737c
OnyxBot <michael.drogalis@onyxplatform.org>
2016-07-01 13:36:53 +0000

    Upgrade to 0.9.7-alpha12.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject onyx-starter "0.9.7-alpha9"
+(defproject onyx-starter "0.9.7-alpha12"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.371"]
-                 [org.onyxplatform/onyx "0.9.7-alpha9"]
+                 [org.onyxplatform/onyx "0.9.7-alpha12"]
                  [com.stuartsierra/component "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :plugins [[lein-update-dependency "0.1.2"]
