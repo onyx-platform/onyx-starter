fa12ae9257a9e7c8ca8a06d32a86f0deedb1f80e
OnyxBot <michael.drogalis@onyxplatform.org>
2016-08-25 20:21:25 +0000

    Update dependency org.onyxplatform/onyx to version 0.9.10-beta1.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.371"]
-                 [org.onyxplatform/onyx "0.9.10-20160825_200126-g269f716"]
+                 [org.onyxplatform/onyx "0.9.10-beta1"]
                  [com.stuartsierra/component "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :plugins [[lein-update-dependency "0.1.2"]
